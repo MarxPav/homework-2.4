fd22579f4da8eb09bfe8c1c9937a8762dd231c46
Karaby <fablelite11@gmail.com>
2024-05-05 22:26:45 +0300

    Домашка сделана

--- src/main/java/ru/skypro/homework/calculator/service/impl/CalculatorServiceImpl.java
+++ src/main/java/ru/skypro/homework/calculator/service/impl/CalculatorServiceImpl.java
@@ -12,9 +12,7 @@ public class CalculatorServiceImpl implements CalculatorService {
     }
 
     @Override
-    public Integer minus(Integer num1, Integer num2) {
-        return num1 - num2;
-    }
+    public Integer minus(Integer num1, Integer num2) {return num1 - num2;}
 
     @Override
     public Integer multiply(Integer num1, Integer num2) {
